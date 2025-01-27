be84d3f29f172aa9984c56da57651de9ce005a9f
ianstrang2 <ian.e.strang@gmail.com>
2025-01-27 15:27:30 +0000

    Fix build setup

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6Wr1eGDNy/hUlvAfpUk0XuNKLEKpbI+xPgl1dTPcEFM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
