bd378b1b991405389e2f77d7e537568cdad81de6
ianstrang2 <ian.e.strang@gmail.com>
2025-04-08 08:37:38 +0100

    roigue file

--- src/components/admin/TeamAlgorithm.tsx
+++
@@ -1,8 +0,0 @@
-SELECT 
-    t.tgname AS trigger_name,
-    CASE WHEN t.tgenabled = 'O' THEN 'ON' ELSE 'OFF' END AS status,
-    c.relname AS table_name
-FROM pg_trigger t
-JOIN pg_class c ON t.tgrelid = c.oid
-WHERE c.relname IN ('matches', 'player_matches')
-ORDER BY c.relname, t.tgname; 
