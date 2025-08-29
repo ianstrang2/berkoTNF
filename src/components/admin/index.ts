6cd017378eba8dadbca180050f26b44989de1fa4
ianstrang2 <ian.e.strang@gmail.com>
2025-08-29 14:29:32 +0100

    fixed

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -1,7 +1,7 @@
 export { FantasyPointsSetup, MatchReportSetup, MatchSettingsSetup } from './AppSetup';
 export { default as PlayerSlot } from './player/PlayerSlot.component';
 export { default as AttributeGuide } from './player/AttributeGuide.component';
-export { default as PlayerRatings } from './player/PlayerRatings.component';
+
 export { default as TeamTemplates } from './team/TeamTemplates.component';
 export { default as PlayerManager } from './player/PlayerManager.component';
 export { default as AppConfig } from './config/AppConfig.component';
